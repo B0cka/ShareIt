539a2991259cb3bd211b6e9e6398c1df8beb5ce3
B0cka <Bocka1423@yandex.ru>
2025-04-29 21:02:38 +0300

    firs commit

---
+++ src/main/java/ru/practicum/Application.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "ru.practicum")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
